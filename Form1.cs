dab8919bf6d6a2e6f89c940d2690e582136c4454
Joan <Joan@DESKTOP-D4KOPVH>
2024-10-20 20:34:35 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_Laboratory_Exercise_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string itemName = txtItemName.Text;
+                double price = Convert.ToDouble(txtPrice.Text);
+                int quantity = Convert.ToInt32(txtQuantity.Text);
+                double discount = Convert.ToDouble(txtDiscount.Text);
+                double discountValue = discount * 0.01;
+                double discountedPrice = price * (1 - discountValue);
+                double totalAmount = discountedPrice * quantity;
+
+                lblTotalAmount.Text = totalAmount.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("erorr try again: " + ex.Message);
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(lblTotalAmount.Text))
+                {
+                    MessageBox.Show("Please compute first");
+                    return;
+                }
+
+
+                double totalAmount = Convert.ToDouble(lblTotalAmount.Text);
+
+                if (string.IsNullOrEmpty(txtPaymentReceived.Text))
+                {
+                    MessageBox.Show("Please enter the payment ");
+                    return;
+                }
+
+                double paymentReceived = Convert.ToDouble(txtPaymentReceived.Text);
+
+                if (paymentReceived < totalAmount)
+                {
+                    MessageBox.Show("kulang po bayad nyo ");
+                }
+                else
+                {
+                    double change = paymentReceived - totalAmount;
+                    lblChange.Text = change.ToString("F2");
+
+                    MessageBox.Show("salamat po!    " + change.ToString("F2"));
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid input");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 loginForm = new Form2();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void exitApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 loginForm = new Form2();
+            loginForm.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_Laboratory_Exercise_2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Text;
+
+            if (username == "skibidi" && password == "1234")
+            {
+                MessageBox.Show("Welcome, Boss Anjo!");
+                Form1 purchaseForm = new Form1();
+                purchaseForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password. Please try again.");
+            }
+        }
+        public class UserAccount
+        {
+            public string FullName { get; set; }
+            public string UserName { get; set; }
+            public string UserPassword { get; set; }
+
+            public UserAccount(string name, string uName, string password)
+            {
+                FullName = name;
+                UserName = uName;
+                UserPassword = password;
+            }
+
+            public bool ValidateLogin(string uName, string password)
+            {
+                return (UserName == uName && UserPassword == password);
+            }
+
+            public string GetFullName()
+            {
+                return FullName;
+            }
+        }
+
+        public class Cashier : UserAccount
+        {
+            public string Department { get; set; }
+
+            public Cashier(string name, string department, string uName, string password)
+                : base(name, uName, password)
+            {
+                Department = department;
+            }
+
+            public string GetDepartment()
+            {
+                return Department;
+            }
+        }
+    }
+    
+}
